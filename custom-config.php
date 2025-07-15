c572f47cb4abe2873850f1d4c78dbc10048e0583
SamShoberWork <62314004+SamShoberWork@users.noreply.github.com>
2025-07-15 10:33:57 -0400

    Create custom-config.php

---
+++ custom-config.php
@@ -0,0 +1,31 @@
+# same as config.inc.php
+
+<?php
+
+// DBMS credentials
+$_DVWA = array();
+$_DVWA[ 'db_server' ]   = 'localhost';
+$_DVWA[ 'db_database' ] = 'dvwa';
+$_DVWA[ 'db_user' ]     = 'root';
+$_DVWA[ 'db_password' ] = 'p@ssw0rd';
+
+// Security level: low, medium, high, impossible
+$_DVWA[ 'default_security_level' ] = 'low';
+
+// ReCAPTCHA settings (optional)
+$_DVWA[ 'recaptcha_public_key' ]  = '';
+$_DVWA[ 'recaptcha_private_key' ] = '';
+
+// PHPIDS (Intrusion Detection System)
+$_DVWA[ 'phpids_enabled' ] = false;
+
+// Default DVWA language
+$_DVWA[ 'default_language' ] = 'en';
+
+// Allow/Disallow file uploads
+$_DVWA[ 'allow_file_upload' ] = true;
+
+// Enable/Disable DVWA login captcha
+$_DVWA[ 'enable_captcha' ] = false;
+
+?>
